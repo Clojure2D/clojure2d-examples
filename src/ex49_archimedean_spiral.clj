9781e05cb726cf50e661bd8a0fcbcd714802d185
Jake <jake.ashdown@protonmail.com>
2018-11-15 20:50:52 +0000

    Fix bugs in ex49 and set radius to oscillate
    * Render smoothly
    * Replace Vec2. with vector
    * Refactor
    * Add a factor of sin(theta) to the radius

--- src/ex49_archimedean_spiral.clj
+++ src/ex49_archimedean_spiral.clj
@@ -1,15 +1,13 @@
 (ns ex49-archimedean_spiral
-  "Generates an archimedean spiral"
+  "Generates an archimedean spiral with an oscillating radius"
   (:require [clojure2d.core :refer :all]
-            [fastmath.core :as m])
-    (:import [fastmath.vector Vec2]))
+            [fastmath.core :as m]
+            [fastmath.vector :as v]))
 
 ;; be sure everything is fast as possible
 (set! *warn-on-reflection* true)
 (set! *unchecked-math* :warn-on-boxed)
 (m/use-primitive-operators)
-
-(def ^:const ^long fps 60)
 
 (def ^:const ^long w 800)
 (def ^:const ^long h 800)
@@ -17,23 +15,27 @@
 (def ^:const ^double x0 400)
 (def ^:const ^double y0 400)
 
-(def ^:const ^double tStep 0.5)
+(def ^:const ^double tStep 0.01)
 
-(def ^:const ^double tAngleScale (/ m/PI 2))
-(def ^:const ^double tRadiusScale 10)
+(def ^:const ^double tAngleScale m/HALF_PI)
+(def ^:const ^double tRadiusScale 10.0)
 
-(defn draw [canvas window ^long frame _]
-    (let [field (get-state window)
-          t     ^double (/ frame fps)]
-      (-> canvas
-          (set-background :black 50)
-          (set-color :white 80))
-      (let [p (for [^double theta (range 0 t tStep)] (Vec2.
-                  (+ x0 (* (m/cos (* theta ^double tAngleScale)) (* theta tRadiusScale)))
-                  (+ y0 (* (m/sin (* theta ^double tAngleScale)) (* theta tRadiusScale)))))]
-        (path-bezier canvas p false true))))
+(def ^:const ^double ampFactor 0.3)
+(def ^:const ^double periodFactor 16)
 
-(def window (show-window {:canvas (canvas w h)
-                          :window-name "Archimedean spiral"
-                          :draw-fn draw
-                          :fps fps}))
+(defn draw [canvas window ^double frame _]
+    (let [t1 ^double (/ frame 60.0)]
+        (-> canvas
+            (set-background :black 50)
+            (set-color :white 80))
+        (let [p (for [^double t (range 0.0 t1 tStep)]
+                  (let [theta (* t tAngleScale)
+                        r (* (+ 1 (* ampFactor (m/sin (* periodFactor theta)))) (* t tRadiusScale))]
+                    (vector (+ x0 (* (m/cos theta) r))
+                            (+ y0 (* (m/sin theta) r)))
+                    ))]
+        (path canvas p))))
+
+(def window (show-window {:canvas (canvas w h :highest)
+                          :window-name "Archimedean spiral with an oscillating radius"
+                          :draw-fn draw}))
