6994063d3cb98423779c15a17ab64266d86ad655
tsl <tomeksul@gmail.com>
2019-01-17 22:21:36 +0100

    xgboost 52 examples

--- src/52_ml_glitch.clj
+++ src/52_ml_glitch.clj
@@ -55,7 +55,7 @@
 (def data-labels (map second samples))
 
 ;; train classifier
-;; (def cl (cl/ada-boost {:number-of-trees 10 :max-nodes 40} data data-labels))
+(def cl (cl/ada-boost {:number-of-trees 10 :max-nodes 40} data data-labels))
 ;; (def cl (cl/decision-tree {:split-rules :entropy :max-nodes 10 :node-size 50} data data-labels))
 ;; (def cl (cl/gradient-tree-boost data data-labels)) ;; slow!!!
 ;; (def cl (cl/knn data data-labels))
@@ -67,7 +67,8 @@
 ;; (def cl (cl/random-forest {:node-size 50 :number-of-trees 50 :subsample 0.9} data data-labels))
 ;; (def cl (cl/rbf-network {:distance dist/manhattan} data data-labels))
 ;; (def cl (cl/rbf-network {:rbf (take 9 (cycle [(rbf/rbf :inverse-quadratic) (rbf/rbf :wu) (rbf/rbf :thinplate)])) :distance dist/chebyshev} data data-labels))
-(def cl (cl/svm {:kernel (kernel/kernel :hyperbolic-tangent) :tolerance 0.1 :c-or-cp 10.0} data data-labels))
+;; (def cl (cl/xgboost data data-labels))
+;; (def cl (cl/svm {:kernel (kernel/kernel :hyperbolic-tangent) :tolerance 0.1 :c-or-cp 10.0} data data-labels))
 
 (def c (canvas (width img) (height img)))
 (def p (p/clone-pixels img))
