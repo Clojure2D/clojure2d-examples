c3bd9b557939a4236a263d1df44a320b5476402c
generateme <generateme.blog@gmail.com>
2020-02-29 22:50:15 +0100

    deps updated

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject clojure2d-examples "1.3.0-alpha2"
+(defproject clojure2d-examples "1.3.0"
   :description "Examples for Clojure2d library"
   :url "https://github.com/Clojure2D/clojure2d-examples"
   :license {:name "The Unlicense"
             :url "http://unlicense.org"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [net.littleredcomputer/sicmutils "0.12.0"]
-                 [generateme/fastmath "1.5.0-alpha4"]
-                 [clojure2d "1.3.0-alpha2"]]
+                 [generateme/fastmath "1.5.2"]
+                 [clojure2d "1.3.0"]]
   
   :repl-options {:timeout 120000}
   :target-path "target/%s"
