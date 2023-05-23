4ff25b3d21dbc71ade5fc79e045f9bdf67e2f0fd
GenerateMe <generateme.blog@gmail.com>
2023-05-23 23:58:27 +0200

    some ex59 improvements

--- src/ex59_plasmas.clj
+++ src/ex59_plasmas.clj
@@ -15,13 +15,40 @@
 
 (def ^:const SIZE 800)
 
+(defn headings ^double [p1 p2] (m/+ (v/heading p1) (v/heading p2)))
+(defn heading ^double [p1 p2] (v/heading (v/add p1 p2)))
+(defn magnitudes ^double [p1 p2] (m/+ (v/mag p1) (v/mag p2)))
+(defn magnitude ^double [p1 p2] (v/mag (v/add p1 p2)))
+(defn dist ^double [p1 p2] (v/dist p1 p2))
+(defn dist-abs ^double [p1 p2] (v/dist-abs p1 p2))
+(defn dist-ang ^double [p1 p2] (v/dist-ang p1 p2))
+(defn dist-emd ^double [p1 p2] (v/dist-emd p1 p2))
+(defn dist-cheb ^double [p1 p2] (v/dist-cheb p1 p2))
+(defn dist-canberra ^double [p1 p2] (v/dist-canberra p1 p2))
+(defn dot ^double [p1 p2] (v/dot p1 p2))
+(defn cross ^double [p1 p2] (v/cross p1 p2))
+
+(def primitive-functions
+  {:headings headings
+   :magnitudes magnitudes
+   :heading heading
+   :magnitude magnitude
+   :dot dot
+   :cross cross
+   :dist dist
+   :dist-abs dist-abs
+   :dist-emd dist-emd
+   :dist-cheb dist-cheb
+   :dist-ang dist-ang
+   :dist-canberra dist-canberra})
+
 (defn make-config []
-  (binding [f/*skip-random-fields* true]
+  (binding [f/*skip-random-fields* (r/brand 0.8)]
     {:f1 (f/random-configuration)
      :f2 (f/random-configuration)
-     :heading-scale (r/drand 0.1 2.0)
-     :mag-scale (r/drand 0.1 2.0)
-     :dot-scale (r/drand 0.1 2.0)
+     :scale1 (r/drand 0.1 2.0)
+     :scale2 (r/drand 0.1 2.0)
+     :scale3 (r/drand 0.1 2.0)
      :n1 (r/random-noise-cfg)
      :n2 (r/random-noise-cfg)
      :n3 (r/random-noise-cfg)
@@ -32,19 +59,21 @@
      :in-rot (r/drand m/TWO_PI)
      :in-shift (v/generate-vec2 #(r/drand -1.0 1.0))
      :colorspace (rand-nth c/colorspaces-list) ;; random colorspace
-     :indices (vec (shuffle (range 3)))
+     :fns (take 3 (shuffle (keys primitive-functions)))
      :pow1 (r/drand 0.5 5.0)
      :pow2 (r/drand 0.5 5.0)
      :pow3 (r/drand 0.5 5.0)}))
 
 (defn plasma->buf
-  [{:keys [f1 f2 ^double heading-scale ^double mag-scale ^double dot-scale
+  [{:keys [f1 f2 ^double scale1 ^double scale2 ^double scale3
            n1 n2 n3 ^double in-scale ^double in-rot ^double in-shift
            colorspace ^double n1-scale ^double n2-scale ^double n3-scale
-           indices ^double pow1 ^double pow2 ^double pow3]
+           ^double pow1 ^double pow2 ^double pow3
+           fns]
     :as config}]
   (pp/pprint (dissoc config :n1 :n2 :n3 :f1 :f2))
   (let [-in-scale (- in-scale)
+        [fn1 fn2 fn3] (map primitive-functions fns)
         field1 (f/combine f1)
         field2 (f/combine f2)
         noise1 (r/random-noise-fn n1)
@@ -52,7 +81,7 @@
         noise3 (r/random-noise-fn n3)
         [_ convert-from] (c/colorspaces* colorspace)
         buf (p/renderer SIZE SIZE :gaussian 2.05 1.0) ;; blur a little bit
-        js (cons [0.0 0.0] (take 4 (r/jittered-sequence-generator :r2 2))) ;; 5 points per pixel
+        js (cons [0.0 0.0] (take 5 (r/jittered-sequence-generator :r2 2))) ;; 6 points per pixel
         is (range (count js))
         xss (mapv first js)
         yss (mapv second js)]
@@ -63,28 +92,24 @@
                   ^double ys (yss i) ;; shift y
                   xx (m/norm (m/+ x xs -0.5) 0 SIZE -in-scale in-scale) ;; scale screen to field coords
                   yy (m/norm (m/+ y ys -0.5) 0 SIZE -in-scale in-scale)
-                  in (-> (v/vec2 xx yy) ;; input
+                  in (-> (v/vec2 xx yy)    ;; input
                          (v/rotate in-rot) ;; rotate
                          (v/add in-shift)) ;; shift
-                  p1 (field1 in) ;; apply field 1
-                  p2 (field2 in) ;; apply field 2
-                  c1 (* 255.0 (m/pow (m/abs (m/sin (m/* (m/+ (v/heading p1)
-                                                             (v/heading p2)
+                  p1 (field1 in)           ;; apply field 1
+                  p2 (field2 in)           ;; apply field 2
+                  c1 (* 255.0 (m/pow (m/abs (m/sin (m/* (m/+ ^double (fn1 p1 p2)
                                                              (m/* n1-scale
                                                                   ^double (noise1 (p1 0) (p1 1))))
-                                                        heading-scale))) pow1))
-                  c2 (* 255.0 (m/pow (m/abs (m/sin (m/* (m/+ (v/mag p1)
-                                                             (v/mag p2)
+                                                        scale1))) pow1))
+                  c2 (* 255.0 (m/pow (m/abs (m/sin (m/* (m/+ ^double (fn2 p1 p2)
                                                              (m/* n2-scale
                                                                   ^double (noise2 (p2 0) (p2 1))))
-                                                        mag-scale))) pow2))
-                  c3 (* 255.0 (m/pow (m/abs (m/sin (m/* (m/+ (v/dot p1 p2)
+                                                        scale2))) pow2))
+                  c3 (* 255.0 (m/pow (m/abs (m/sin (m/* (m/+ ^double (fn3 p1 p2)
                                                              (m/* n3-scale
                                                                   ^double (noise3 (p1 1) (p2 0) 0.3456)))
-                                                        dot-scale))) pow3))
-                  col (-> (v/vec3 c1 c2 c3)
-                          (v/permute indices)
-                          convert-from)]]
+                                                        scale3))) pow3))
+                  col (-> (v/vec3 c1 c2 c3) convert-from)]]
       (p/set-color! buf x y col))
     buf))
 
