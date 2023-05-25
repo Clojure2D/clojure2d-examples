62a6eef40ea3cfc56a1e842d9a61b59c9438ea03
GenerateMe <generateme.blog@gmail.com>
2023-05-25 13:16:07 +0200

    save the config, ex59

--- src/ex59_plasmas.clj
+++ src/ex59_plasmas.clj
@@ -13,6 +13,9 @@
 (set! *warn-on-reflection* true)
 (set! *unchecked-math* :warn-on-boxed)
 
+;; store configs
+(defonce configs (atom {}))
+
 (def ^:const SIZE 800)
 
 (defn headings ^double [p1 p2] (m/+ (v/heading p1) (v/heading p2)))
@@ -71,7 +74,7 @@
      :n3-scale (r/drand m/PI)
      :in-scale (r/drand 0.1 3.0)
      :in-rot (r/drand m/TWO_PI)
-     :in-shift (v/generate-vec2 #(r/drand -1.0 1.0))
+     :in-shift [(r/drand -1.0 1.0) (r/drand -1.0 1.0)]
      :colorspace (rand-nth c/colorspaces-list) ;; random colorspace
      :fns (repeatedly 3 #(rand-nth (keys primitive-functions)))
      :pow1 (r/drand 0.3 7.0)
@@ -81,7 +84,7 @@
 
 (defn plasma->buf
   [{:keys [f1 f2 ^double scale1 ^double scale2 ^double scale3
-           n1 n2 n3 ^double in-scale ^double in-rot ^double in-shift
+           n1 n2 n3 ^double in-scale ^double in-rot in-shift
            colorspace ^double n1-scale ^double n2-scale ^double n3-scale
            ^double pow1 ^double pow2 ^double pow3
            fns]}]
@@ -92,6 +95,7 @@
         noise1 (r/random-noise-fn n1)
         noise2 (r/random-noise-fn n2)
         noise3 (r/random-noise-fn n3)
+        in-shift (apply v/vec2 in-shift)
         [_ convert-from] (c/colorspaces* colorspace)
         buf (p/renderer SIZE SIZE :gaussian 1.45 1.0) ;; blur a little bit
         js (take 4 (drop (r/irand 100) (r/jittered-sequence-generator :r2 2))) ;; 4 points per pixel
@@ -136,10 +140,17 @@
    (c2d/get-image (p/to-pixels buf {:splats? true :saturation saturation}))
    noverlay))
 
+;; store the file name
+(def file-name (c2d/next-filename "results/ex59/"))
+
+;; store config
 (def config (let [config (make-config)]
+            (pp/pprint file-name)
+            (swap! configs assoc file-name config) ;; save config to an atom
             (pp/pprint (dissoc config :n1 :n2 :n3 :f1 :f2))
             config))
 
+;; render image
 (def buffer (->> (-> c2d/available-cores
                    (m// 2)
                    (m/max 1)
@@ -148,9 +159,14 @@
                (map deref) ;; get results and combine
                (reduce p/merge-renderers)))
 
+;; build the image (raw -> image, saturation adjustment, adding noise)
 (def image (draw-plasma buffer config))
+
+;; show the result
 (u/show-image image)
 
 (comment
-  (c2d/save image (c2d/next-filename "results/ex59/" ".jpg"))
+  (do
+    (spit (str file-name ".edn") config) ;; save configuration
+    (c2d/save image (str file-name ".jpg"))) ;; save the result
   )
