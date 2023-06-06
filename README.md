7ea7e2e62e295f3b21e9b2afb5add0f5d46735ef
GenerateMe <generateme.blog@gmail.com>
2023-06-06 15:51:20 +0200

    ex61

--- README.md
+++ README.md
@@ -71,6 +71,7 @@ In Emacs/Cider: C-c C-k
 * ex58 - Circle Inversion art
 * ex59 - Plasmas
 * ex60 - Metal sheets
+* ex61 - Horizontals
 * NOC - [Nature of Code](https://github.com/shiffman/The-Nature-of-Code-Examples/) Clojure2d version. Most interesting:
   * ch02/attraction-many-2-7.clj
   * ch02/fluidresistance-2-5.clj
---
+++ src/ex61_horizontals.clj
@@ -0,0 +1,44 @@
+(ns ex61-horizontals
+  (:require [fastmath.fields :as f]
+            [clojure2d.core :as c2d]
+            [clojure2d.color :as c]
+            [clojure2d.extra.utils :as u]
+            [fastmath.core :as m]
+            [fastmath.vector :as v]
+            [fastmath.random :as r]
+            [clojure2d.extra.overlays :as o]))
+
+(def no (o/noise-overlay 600 600))
+
+(def res (binding [f/*skip-random-fields* (r/brand 0.9)]
+         (let [b1 (r/drand -12.0 -3.0)
+               b2 (- b1)
+               field (f/random-field)
+               #_#_pal (c/palette) 
+               #_#_pal [:black (c/gray 100) :white (c/gray 127.5) :black (c/gray 155) :white]
+               pal [:black (c/gray 100) (c/gray 155) :white]
+               palcnt (dec (count pal))]
+           (c2d/with-canvas [c (c2d/canvas 600 600)]
+             (c2d/set-background c (c/gray 240))
+             (doseq [y (range 600)
+                     :let [yy (m/norm y 0.0 600.0 b1 b2)
+                           step (-> (v/vec2 b1 yy) field v/mag m/sin m/sq inc (* 0.5))]]
+               (loop [xx (double b1)]
+                 (when (< xx b2)
+                   (let [fv (field (v/vec2 xx yy))
+                         nxx (+ xx step)
+                         x (m/norm xx b1 b2 0.0 600.0)
+                         nx (m/norm nxx b1 b2 0.0 600.0)
+                         h (m/sin (v/heading fv)) ;; v/mag
+                         pv (m/round (m/norm h -1.0 1.0 0.0 palcnt))]
+                     (c2d/set-color c (pal pv))
+                     (c2d/line c x y nx y)
+                     (recur nxx)))))
+             (let [img (o/render-noise
+                        (c2d/to-image c) no)]
+               (u/show-image img)
+               img)))))
+
+(comment
+  (c2d/save res (c2d/next-filename "results/ex61/" ".jpg"))
+  )
