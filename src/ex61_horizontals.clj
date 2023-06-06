12414bd3718cf1ad704ba331a3e5d2df408d7ad0
GenerateMe <generateme.blog@gmail.com>
2023-06-06 15:56:53 +0200

    ex61 comments

--- src/ex61_horizontals.clj
+++ src/ex61_horizontals.clj
@@ -11,30 +11,31 @@
 (def no (o/noise-overlay 600 600))
 
 (def res (binding [f/*skip-random-fields* (r/brand 0.9)]
-         (let [b1 (r/drand -12.0 -3.0)
-               b2 (- b1)
-               field (f/random-field)
-               #_#_pal (c/palette) 
+         (let [b1 (r/drand -12.0 -3.0) ;; left vector field boundary
+               b2 (- b1) ;; right boundary
+               field (f/random-field) ;; random vector field
+               #_#_pal (c/palette)  
                #_#_pal [:black (c/gray 100) :white (c/gray 127.5) :black (c/gray 155) :white]
-               pal [:black (c/gray 100) (c/gray 155) :white]
-               palcnt (dec (count pal))]
+               pal [:black (c/gray 100) (c/gray 155) :white] ;; some palette
+               palcnt (dec (count pal))] ;; palette count
            (c2d/with-canvas [c (c2d/canvas 600 600)]
              (c2d/set-background c (c/gray 240))
-             (doseq [y (range 600)
-                     :let [yy (m/norm y 0.0 600.0 b1 b2)
+             (doseq [y (range 600) ;; traverse vertically, line by line
+                     :let [yy (m/norm y 0.0 600.0 b1 b2) ;; screen to field coords
+                           ;; calculate line width (horizontal step) as some value based in field vector magnitude
                            step (-> (v/vec2 b1 yy) field v/mag m/sin m/sq inc (* 0.5))]]
-               (loop [xx (double b1)]
+               (loop [xx (double b1)] ;; traverse horizontally, step-size
                  (when (< xx b2)
-                   (let [fv (field (v/vec2 xx yy))
-                         nxx (+ xx step)
-                         x (m/norm xx b1 b2 0.0 600.0)
+                   (let [fv (field (v/vec2 xx yy)) ;; vector field value
+                         nxx (+ xx step) ;; line segment end
+                         x (m/norm xx b1 b2 0.0 600.0) ;; field to screen coords
                          nx (m/norm nxx b1 b2 0.0 600.0)
-                         h (m/sin (v/heading fv)) ;; v/mag
-                         pv (m/round (m/norm h -1.0 1.0 0.0 palcnt))]
-                     (c2d/set-color c (pal pv))
-                     (c2d/line c x y nx y)
+                         h (m/sin (v/heading fv)) ;; v/mag ;; angle of the field vector...
+                         pv (m/round (m/norm h -1.0 1.0 0.0 palcnt))] ;; ... is mapped to the palette
+                     (c2d/set-color c (pal pv)) ;; set color
+                     (c2d/line c x y nx y) ;; draw line segment
                      (recur nxx)))))
-             (let [img (o/render-noise
+             (let [img (o/render-noise ;; apply noise overlay
                         (c2d/to-image c) no)]
                (u/show-image img)
                img)))))
